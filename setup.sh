9c54705e32d0500b6fdf63800b8b2029533b8dd1
abdel_latrache <latreche.abdel.fr@gmail.com>
2025-11-20 08:25:33 +0800

    update

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 cd ~/chess-bot
 source ~/robot_venv/bin/activate
 pip install -r client/requirements.txt
-sudo apt update && sudo apt install -y espeak libcamera-apps
+# sudo apt update && sudo apt install -y espeak libcamera-apps
+python3 -m venv venv
 espeak "Setup complete"
